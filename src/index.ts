56f5e0bea04a79e2bb1a5269af288c32488ba3db
Abdellah Alaoui <als.abdellah@gmail.com>
2021-01-11 14:47:48 +0100

    fix(index): fix default exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import * as inertiaExpressAdapter from './lib/expressAdapter';
-import * as expressFlash from './lib/expressFlash';
-import * as inertiaKoaAdapter from './lib/koaAdapter';
-import * as koaFlash from './lib/koaFlash';
+import inertiaExpressAdapter from './lib/expressAdapter';
+import expressFlash from './lib/expressFlash';
+import inertiaKoaAdapter from './lib/koaAdapter';
+import koaFlash from './lib/koaFlash';
 
 export { inertiaKoaAdapter, koaFlash, inertiaExpressAdapter, expressFlash };
